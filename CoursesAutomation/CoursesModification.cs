ec700d7b3cb225bc0e402747a976b8f3ee03de5d
MischaTobias <49697648+MischaTobias@users.noreply.github.com>
2021-05-11 18:19:10 -0600

    Inicio de programa

---
+++ CoursesAutomation/CoursesModification.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace CoursesAutomation
+{
+    public partial class CoursesModification : Form
+    {
+        public CoursesModification()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSeleccionarArchivo_Click(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
